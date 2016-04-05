80cd42be6ad7c6e68996a070cef66cb40a5dc85d
anyab5 <anyab5@gmail.com>
2016-04-05 12:52:12 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var decompress = require('gulp-decompress');
 
 // Imported from Brand.ai
 var scssURL = 'https://assets.brand.ai/acme-demo/_style-params.scss';
-var iconsURL = 'https://assets.brand.ai/acme-demo-new/icons-zip';
-var imagesURL = 'https://assets.brand.ai/acme-demo-new/images-zip';
+var iconsURL = 'https://assets.brand.ai/acme-demo/icons-zip';
+var imagesURL = 'https://assets.brand.ai/acme-demo/images-zip';
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
